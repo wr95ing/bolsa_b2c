254b64aa9b4613851ae0038ae3f09f20386a2cac
wr95ing <wr95ing@gmail.com>
2026-06-09 12:27:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ async function main() {
   try {
 
     const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
-      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    });
+  credentials: {
+    type: "service_account",
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+});
 
     const sheets = google.sheets({
       version: "v4",
